5b7303be098ef3f29a6573ea61e11cdeac1c2c04
Nick Dyer <nbdyer@gmail.com>
2016-07-13 17:11:54 +0100

    Broadcaster failing to broadcast

--- src/main/java/chatjava/BroadCaster.java
+++ src/main/java/chatjava/BroadCaster.java
@@ -17,7 +17,7 @@ public class BroadCaster {
     public void listenAndRelayMessages(ChatRoom chatRoom) {
         for (Representative rep : chatRoom.getParticipants()) {
             ByteArrayInputStream inputStream = new ByteArrayInputStream(rep.getOutputToRoom().toByteArray());
-            if (inputStream.available() > 0) {
+            while (inputStream.available() > 0) {
                 try {
                     new PrintWriter(output, true).println(new BufferedReader(new InputStreamReader(inputStream)).readLine());
                 } catch (IOException e) {
--- src/test/java/chatjava/ChatRoomTest.java
+++ src/test/java/chatjava/ChatRoomTest.java
@@ -4,6 +4,7 @@ import chatjava.testdoubles.DummyInputStream;
 import chatjava.testdoubles.DummyOutputStream;
 import chatjava.testdoubles.DummyRepresentative;
 import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -17,6 +18,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ChatRoomTest {
+
+    private DummyOutputStream dos;
+    private DummyInputStream dis;
+
+    @Before
+    public void setUp() {
+        dos = new DummyOutputStream();
+        dis = new DummyInputStream();
+    }
 
     @Test
     public void isEmptyWhenCreated() {
@@ -45,13 +55,33 @@ public class ChatRoomTest {
         ByteArrayInputStream input = new ByteArrayInputStream("Broadcast this message!".getBytes());
         BroadCaster broadCaster = new BroadCaster();
         ChatRoom chatRoom = ChatRoom.empty();
-        Representative nick = new Representative("Nick", new CommunicationLink(input, new DummyOutputStream()), new CommunicationLink(new DummyInputStream(), new ByteArrayOutputStream()));
+        Representative nick = new Representative("Nick", new CommunicationLink(input, dos), new CommunicationLink(dis, new ByteArrayOutputStream()));
         chatRoom.add(nick);
 
         nick.listenAndRelayMessages();
         broadCaster.listenAndRelayMessages(chatRoom);
 
         assertEquals("Broadcast this message!\n", broadCaster.getOutput().toString());
+    }
+
+    @Test
+    public void aRepresentativesMessageToTheBroadcasterIsSentToOtherRepresentatives() {
+        ByteArrayInputStream input = new ByteArrayInputStream("Broadcast this message!".getBytes());
+        ByteArrayOutputStream nicksReceivedMessage = new ByteArrayOutputStream();
+        ByteArrayOutputStream mateusReceivedMessage = new ByteArrayOutputStream();
+        BroadCaster broadCaster = new BroadCaster();
+        ChatRoom chatRoom = ChatRoom.empty();
+        Representative nick = new Representative("Nick", new CommunicationLink(input, nicksReceivedMessage), new CommunicationLink(new ByteArrayInputStream(broadCaster.getOutput().toByteArray()), new ByteArrayOutputStream()));
+        Representative mateu = new Representative("Mateu", new CommunicationLink(dis, mateusReceivedMessage), new CommunicationLink(new ByteArrayInputStream(broadCaster.getOutput().toByteArray()), new ByteArrayOutputStream()));
+        chatRoom.add(nick);
+        chatRoom.add(mateu);
+
+        nick.listenAndRelayMessages();
+        mateu.listenAndRelayMessages();
+        broadCaster.listenAndRelayMessages(chatRoom);
+
+        assertEquals("Broadcast this message!\n", nicksReceivedMessage.toString());
+        assertEquals("Broadcast this message!\n", mateusReceivedMessage.toString());
     }
 
 }
